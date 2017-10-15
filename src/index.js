e7ed6b02ff9f607c121d4c5ff9784d4cf395a334
Phillip-Juan <flaapjan@zailab.com>
2017-10-15 13:56:39 +0200

    remove constructor

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 const uuid = require('uuid/v1');
 
-let __custom_elements__ = {};
+const __custom_elements__ = {};
 
 class CustomElement {
-  constructor(config) {
-    this.init(config);
-  }
-
-  init(config) {
+  config (config) {
     this.setupUsables(config);
     this.findInstances();
   }
 
-  setupUsables(config) {
+  setupUsables (config) {
     console.log(' config >>>> ', config);
     this.shadowRoots = {};
     this.hosts = {};
@@ -21,7 +17,7 @@ class CustomElement {
     this.viewModel = config.viewModel;
   }
 
-  findInstances() {
+  findInstances () {
     const _elements_ = document.querySelectorAll(this.tagName);
 
     if (_elements_ && _elements_.length) {
@@ -29,15 +25,15 @@ class CustomElement {
     }
   }
 
-  render(_elements_) {
+  render (_elements_) {
     if (this.hasSource()) {
-      for (let el of _elements_) {
+      for (const el of _elements_) {
         this.renderSingleElement(el);
       }
     }
   }
 
-  renderSingleElement(el) {
+  renderSingleElement (el) {
     const _id = uuid();
 
     this.addHost(_id, el);
@@ -48,7 +44,8 @@ class CustomElement {
           $(this.shadowRoots[_id]).load(this.template);
 
           console.log(' loading >>>> ', this.template);
-          var script = document.createElement('script');
+          const script = document.createElement('script');
+
           script.type = 'text/javascript';
           script.src = this.viewModel;
           this.shadowRoots[_id].appendChild(script);
@@ -60,44 +57,50 @@ class CustomElement {
       );
   }
 
-  addHost(_id, el) {
+  addHost (_id, el) {
     el.id = _id;
     this.hosts[_id] = el;
   }
 
-  createShadowDom(_id) {
+  createShadowDom (_id) {
     return new Promise((resolve, reject) => {
       if (this.shadowRoot) {
         console.log('%c shadow root cannot be created on a host which already hosts a shadow tree. ', 'color:#9D2933;');
         reject();
+
         return;
       }
 
       try {
-        this.shadowRoots[_id] = this.hosts[_id].attachShadow({ mode: 'open' });
+        this.shadowRoots[_id] = this.hosts[_id].attachShadow({mode: 'open'});
         resolve();
-      } catch (e) {
+      }
+      catch (e) {
         console.log('%c something is wrong with your custom element! this is not a problem with flaap-gallery, but between the chair and the computer. ', 'color:#9D2933;');
         reject();
       }
     });
   }
 
-  hasSource() {
-    if (this.template) return true;
+  hasSource () {
+    if (this.template) {
+      return true;
+    }
     console.log('%c no source specified for component ', 'color:#9D2933;');
+
     return;
   }
 }
 
 class CustomComponent {
-  constructor(config) {
+  constructor (config) {
     if (__custom_elements__[config.tagName]) {
       __custom_elements__[config.tagName].init();
+
       return;
     }
     __custom_elements__[config.tagName] = new CustomElement(config);
   }
 }
 
-exports.customElement = CustomComponent;
+exports.customElement = new CustomComponent();
